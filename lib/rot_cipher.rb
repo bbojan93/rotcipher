f076236a4ed38fa3241f6985901f5d32084650c5
Bojan Babic <bojance93@gmail.com>
2017-10-04 12:52:39 +0200

    Add initialize method

--- lib/rot_cipher.rb
+++ lib/rot_cipher.rb
@@ -1,30 +1,32 @@
 class StringCipher
 
-  def self.cipher(string, key)
+  def initialize(key)
+    @key = key
+  end
+
+  def cipher(string)
     alphabet = ('a'..'z').to_a
 
     newstring = ''
 
-      string.each_char do |char|
+    string.each_char do |char|
 
-        if char !~ /\w/
-          newstring += char
-        end
+      if char !~ /\w/ || char  =~ /\d/
+        newstring += char
+      end
 
-        if char =~ /\d/
-          newstring += char
-        end
-
-        alphabet.each.with_index do |letter, index|
-          if letter == char
-            newindex = index + key - 26
-            newstring += alphabet[newindex]
-          elsif letter.upcase == char
-            newindex = index + key - 26
-            newstring += alphabet[newindex].upcase
-          end
+      alphabet.each.with_index do |letter, index|
+        if letter == char
+          newindex = index + @key - 26
+          newstring += alphabet[newindex]
+        elsif letter.upcase == char
+          newindex = index + @key - 26
+          newstring += alphabet[newindex].upcase
         end
       end
-      return newstring
+
+    end
+
   end
+
 end
